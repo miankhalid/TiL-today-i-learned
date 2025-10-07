807529c5a5d28836991db3e03ea403e4b4b74192
Mian Khalid <mian.khalid@arbisoft.com>
2025-10-07 09:18:55 +0500

    Remove unused import

--- react-native/SampleProfileApp/src/todo-app/HomeTab.jsx
+++ react-native/SampleProfileApp/src/todo-app/HomeTab.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView, Text, useColorScheme, View } from 'react-native';
+import { ScrollView, Text, useColorScheme } from 'react-native';
 import createStyles from '../themes/Styles';
 import TodoItem from './components/TodoItem';
 

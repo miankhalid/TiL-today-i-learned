205e11bdcf3dd6a3fbd5e8484ebe7c59a58452b9
Mian Khalid <mian.khalid@arbisoft.com>
2025-10-07 09:55:54 +0500

    useReducer instead of useState

--- react-native/SampleProfileApp/src/todo-app-v3/context/TodoProvider.js
+++ react-native/SampleProfileApp/src/todo-app-v3/context/TodoProvider.js
@@ -1,35 +1,46 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import Todo from '../models/Todo';
 import { TodoContext } from './TodoContext';
 
+const todoReducer = (state, action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return [...state, new Todo(state.length + 1, action.payload)];
+        case 'MARK_COMPLETE':
+            return state.map(todo =>
+                todo.id === action.payload ? todo.markDone() : todo
+            );
+        case 'DELETE_TODO':
+            return state.filter(todo => todo.id !== action.payload);
+        case 'EDIT_TODO':
+            return state.map(todo =>
+                todo.id === action.payload.id ? todo.updateText(action.payload.newText) : todo
+            );
+        default:
+            return state;
+    }
+};
+
 export const TodoProvider = ({ children }) => {
-    const [todos, setTodos] = useState([
+    const [todos, dispatch] = useReducer(todoReducer, [
         new Todo(1, "Item # 1"),
         new Todo(2, "Item # 2"),
     ]);
 
     const addTodo = (text) => {
-        const updatedTodos = [...todos, new Todo(todos.length + 1, text)];
-        setTodos(updatedTodos);
+        dispatch({ type: 'ADD_TODO', payload: text });
     };
 
     const markComplete = (id) => {
-        const updatedTodos = todos.map(todo =>
-            todo.id === id ? todo.markDone() : todo
-        );
-        setTodos(updatedTodos);
+        dispatch({ type: 'MARK_COMPLETE', payload: id });
     };
 
     const deleteTodo = (id) => {
-        const updatedTodos = todos.filter(item => item.id !== id);
-        setTodos(updatedTodos);
+        dispatch({ type: 'DELETE_TODO', payload: id });
     };
 
     const editTodo = (id, newText) => {
-        const updatedTodos = todos.map(item =>
-            item.id === id ? item.updateText(newText) : item
-        );
-        setTodos(updatedTodos);
+        dispatch({ type: 'EDIT_TODO', payload: { id, newText } });
     };
 
     return (
